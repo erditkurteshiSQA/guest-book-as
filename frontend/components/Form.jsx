69f8e102e198d2cd9aa1668610fec7d96f110837
Erdit.Kurteshi <erdit.kurteshi@sqa-consulting.com>
2022-10-03 16:30:57 +0200

    integrated wallet-selector into guest book

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -1,46 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Big from 'big.js';
+import React from "react";
+import Big from "big.js";
 
-export default function Form({ onSubmit, currentUser }) {
+const Form = ({ account, onSubmit }) => {
   return (
     <form onSubmit={onSubmit}>
       <fieldset id="fieldset">
-        <p>Sign the guest book, { currentUser.accountId }!</p>
+        <p>Sign the guest book, {account.account_id}!</p>
         <p className="highlight">
           <label htmlFor="message">Message:</label>
-          <input
-            autoComplete="off"
-            autoFocus
-            id="message"
-            required
-          />
+          <input autoComplete="off" autoFocus id="message" required />
         </p>
         <p>
           <label htmlFor="donation">Donation (optional):</label>
           <input
             autoComplete="off"
-            defaultValue={'0'}
+            defaultValue={"0"}
             id="donation"
-            max={Big(currentUser.balance).div(10 ** 24)}
+            max={Big(account.amount)
+              .div(10 ** 24)
+              .toString()}
             min="0"
             step="0.01"
             type="number"
           />
           <span title="NEAR Tokens">Ⓝ</span>
         </p>
-        <button type="submit">
-          Sign
-        </button>
+        <p>
+          <label htmlFor="multiple">Multiple Transactions:</label>
+          <input id="multiple" type="checkbox" />
+        </p>
+        <button type="submit">Sign</button>
       </fieldset>
     </form>
   );
-}
-
-Form.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  currentUser: PropTypes.shape({
-    accountId: PropTypes.string.isRequired,
-    balance: PropTypes.string.isRequired
-  })
 };
+
+export default Form;
